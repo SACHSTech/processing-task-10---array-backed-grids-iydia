c2e6c50888645027e7a97a19568732b25b2d5cd1
Lydia <75390984+iydia@users.noreply.github.com>
2022-06-11 14:56:41 +0000

    level 2

--- Sketch.java
+++ Sketch.java
@@ -18,6 +18,10 @@ public class Sketch extends PApplet {
   int COLUMN_COUNT = 10;
   int SCREEN_WIDTH = (CELL_WIDTH + MARGIN) * COLUMN_COUNT + MARGIN;
   int SCREEN_HEIGHT = (CELL_WIDTH + MARGIN) * COLUMN_COUNT + MARGIN;
+  int column;
+  int row;
+  // Array Variables
+  int[][] intGrid;
 
   public void settings() {
     // Size call
@@ -26,17 +30,38 @@ public class Sketch extends PApplet {
 
   public void setup() {
     background(0);
+    intGrid = new int[10][10];
   }
 
   public void draw() {
-    for (int column = 0; column < COLUMN_COUNT; column++) {
-      for (int row = 0; row < ROW_COUNT; row++) {
-        rect(CELL_WIDTH*column + MARGIN*column, CELL_HEIGHT*row + MARGIN*row, CELL_WIDTH, CELL_HEIGHT);
+    intGrid[1][5] = 1;
+    for (column = 0; column < COLUMN_COUNT; column++) {
+      for (row = 0; row < ROW_COUNT; row++) {
+        // Draws a green retangle when the value of intGrid[row][column]is equal to 1
+        if(intGrid[row][column] == 1){
+          fill(0, 255, 0);
+            rect(CELL_WIDTH*column + MARGIN*column, 
+            CELL_HEIGHT*row + MARGIN*row, 
+            CELL_WIDTH, CELL_HEIGHT);
+        // Otherwise, draw a white rectangle
+        } else {
+          fill(255);
+            rect(CELL_WIDTH*column + MARGIN*column, 
+            CELL_HEIGHT*row + MARGIN*row, 
+            CELL_WIDTH, CELL_HEIGHT);
+        }
       }
     }
   }
   
   public void mousePressed(){
-    
+    System.out.println("click");
+    for (column = 0; column < COLUMN_COUNT; column++) {
+      for (row = 0; row < ROW_COUNT; row++) {
+        if (dist(MARGIN * (column + 1) + (column * CELL_WIDTH) + ROW_COUNT, MARGIN * (row + 1) + (row * CELL_HEIGHT) + COLUMN_COUNT, mouseX, mouseY) < COLUMN_COUNT ) {
+          System.out.println("mouse coordinates: (" + mouseX + ", " + mouseY + "); grid coordinates: (row:" + row + ", column:" + column + ")");
+        }
+      }
+    }
   }
 }
